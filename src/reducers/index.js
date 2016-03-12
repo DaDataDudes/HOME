6cdb4563959f2feb08ecf968ef10bee3347bb309
Marifel Barbasa <mrbarbasa@gmail.com>
2016-03-12 01:29:25 -1000

    Add firebase and remove counter and todo

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,14 +2,10 @@ import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 // Reducers
 import firebase from 'reducers/firebase';
-import counter from 'reducers/counter';
-import todo from 'reducers/todo';
 import documents from 'reducers/documents';
 import auth from 'reducers/auth';
 
 const rootReducer = combineReducers({
-  counter,
-  todo,
   documents,
   firebase,
   routing,
