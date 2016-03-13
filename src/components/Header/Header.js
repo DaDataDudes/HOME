9d6a941c1836ee5a766c47464a1cb3f7d7a28d96
Emmanuel Pilande <epilande@gmail.com>
2016-03-12 14:25:22 -1000

    Link logo to dashboard

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import { Row, Col } from 'react-flexbox-grid';
 import styles from './Header.css';
 import userIcon from 'assets/user-icon.svg';
@@ -7,7 +8,7 @@ const Header = ({ branding, username }) => (
   <header className={styles.base}>
     <Row>
       <Col xs={6} className={styles.logo}>
-        {branding}
+        <Link to="dashboard">{branding}</Link>
       </Col>
       <Col xs={6} className={styles.user}>
         <img className={styles.userImg} src={userIcon} />
