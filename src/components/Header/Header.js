2cc69c20d76e8cbc12e87030f779d82f931f9f11
Emmanuel Pilande <epilande@gmail.com>
2016-03-12 14:39:13 -1000

    Fix header spacing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { Row, Col } from 'react-flexbox-grid';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import styles from './Header.css';
 import userIcon from 'assets/user-icon.svg';
 
 const Header = ({ branding, username }) => (
   <header className={styles.base}>
-    <Row>
-      <Col xs={6} className={styles.logo}>
-        <Link to="dashboard">{branding}</Link>
-      </Col>
-      <Col xs={6} className={styles.user}>
-        <img className={styles.userImg} src={userIcon} />
-        <p className={styles.username}>{username}</p>
-      </Col>
-    </Row>
+    <Grid fluid={true}>
+      <Row>
+        <Col xs={6} className={styles.logo}>
+          <Link to="dashboard">{branding}</Link>
+        </Col>
+        <Col xs={6} className={styles.user}>
+          <img className={styles.userImg} src={userIcon} />
+          <p className={styles.username}>{username}</p>
+        </Col>
+      </Row>
+    </Grid>
   </header>
 );
 
