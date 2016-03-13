652dca747938ee78bda4abcdafcfbcfdc5d8dba2
Emmanuel Pilande <epilande@gmail.com>
2016-03-12 17:43:25 -1000

    Fix prod build

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -19,21 +19,22 @@ config.module.loaders.push({
     'style-loader',
     'css-loader',
     'postcss-loader'
-  )
+  ),
 }, {
   test: /^((?!\.global).)*\.css$/,
   loader: ExtractTextPlugin.extract(
     'style-loader',
     'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
     'postcss-loader'
-  )
+  ),
 });
 
 config.postcss = function postcss() {
   return [
     require('postcss-modules-values'),
     require('postcss-nested'),
-    require('rucksack-css')
+    require('postcss-import'),
+    require('rucksack-css'),
   ];
 };
 
@@ -42,8 +43,8 @@ config.plugins.push(
   new webpack.optimize.DedupePlugin(),
   new webpack.optimize.UglifyJsPlugin({
     compress: {
-      warnings: false
-    }
+      warnings: false,
+    },
   }),
   new HtmlWebpackPlugin({
     template: 'src/index.html',
@@ -59,13 +60,13 @@ config.plugins.push(
       minifyCSS: true,
       minifyURLs: true,
     },
-    inject: true
+    inject: true,
   }),
   new webpack.DefinePlugin({
     '__DEV__': false,
     'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
+      'NODE_ENV': JSON.stringify('production'),
+    },
   }),
   new ExtractTextPlugin('style.css', { allChunks: true })
 );
