999bdbb96a289db761179354c8fd32112275d149
Emmanuel Pilande <epilande@gmail.com>
2016-03-24 02:40:27 -1000

    Fix map eslint error

--- src/components/charts/Map/Map.js
+++ src/components/charts/Map/Map.js
@@ -4,7 +4,6 @@ import topojson from 'topojson';
 import Svg from 'components/charts/Svg';
 
 const hawaii = require('assets/hawaii-topojson.json');
-let tool;
 
 const quantize = d3.scale.quantize()
   .domain([0, 100])
@@ -32,36 +31,35 @@ function fillColor(total) {
     return '#C0C0C0';
   }
   const color = +total.total;
-    if (color > 9000){
-      return 'rgb(8,48,107)';
-    }
-    if (color > 8000){
-      return 'rgb(8,81,156)';
-    }
-    if (color > 7000){
-      return 'rgb(33,113,181)';
-    }
-    if (color > 6000){
-      return 'rgb(66,146,198)';
-    }
-    if (color > 5000){
-      return 'rgb(107,174,214)';
-    }
-    if (color > 4000){
-      return 'rgb(158,202,225)';
-    }
-    if (color > 3000){
-      return 'rgb(198,219,239)';
-    }
-    if (color > 2000){
-      return 'rgb(222,235,247)';
-    }
-    if (color > 1000){
-      return 'rgb(247,251,255)';
-    }
-    return '#C0C0C0';
+  if (color > 9000) {
+    return 'rgb(8,48,107)';
   }
-
+  if (color > 8000) {
+    return 'rgb(8,81,156)';
+  }
+  if (color > 7000) {
+    return 'rgb(33,113,181)';
+  }
+  if (color > 6000) {
+    return 'rgb(66,146,198)';
+  }
+  if (color > 5000) {
+    return 'rgb(107,174,214)';
+  }
+  if (color > 4000) {
+    return 'rgb(158,202,225)';
+  }
+  if (color > 3000) {
+    return 'rgb(198,219,239)';
+  }
+  if (color > 2000) {
+    return 'rgb(222,235,247)';
+  }
+  if (color > 1000) {
+    return 'rgb(247,251,255)';
+  }
+  return '#C0C0C0';
+}
 
 const Map = ({ updateInfo, totals, info, width = 500, height = 500 }) => {
   const counties = topojson.feature(hawaii, hawaii.objects.hi_ct).features;
@@ -75,11 +73,9 @@ const Map = ({ updateInfo, totals, info, width = 500, height = 500 }) => {
       </div>
       <Svg width={width} height={height}>
         <g className="counties">
-          {counties.map((county, i) => {
-            return (
-              <path key={i} d={pathGenerator(county)} name={county.id} fill={fillColor(totals[i])} className={quantize(randomNum())} stroke={strokeColor()} onMouseOver={updateInfo} />
-            );
-          })}
+          {counties.map((county, i) => (
+            <path key={i} d={pathGenerator(county)} name={county.id} fill={fillColor(totals[i])} className={quantize(randomNum())} stroke={strokeColor()} onMouseOver={updateInfo} />
+            ))}
         </g>
       </Svg>
     </div>
