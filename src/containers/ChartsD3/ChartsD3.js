8fde2ab133493f161e6dd8a2c83466774bac36fa
Emmanuel Pilande <epilande@gmail.com>
2016-03-24 02:14:01 -1000

    Fix eslint errors in chartsD3

--- src/containers/ChartsD3/ChartsD3.js
+++ src/containers/ChartsD3/ChartsD3.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import { firebase } from 'actions/firebase'
+import { firebase } from 'actions/firebase';
 import { getChartData, updateChartData, getChoroInfo, updateChoroInfo } from 'actions/charts';
 import tooltipData from 'seed/counties';
 import Map from 'components/charts/Map';
@@ -19,17 +18,17 @@ class ChartsD3 extends Component {
       x: 0,
       y: 0,
       width: 500,
-      height: 400
-    }
+      height: 400,
+    };
+  }
+
+  componentWillMount() {
+    this.props.dispatch(firebase.registerListeners());
   }
 
   componentDidMount() {
     this.props.dispatch(getChartData());
     this.props.dispatch(getChoroInfo());
-  }
-
-  componentWillMount() {
-    this.props.dispatch(firebase.registerListeners());
   }
 
   _updateData() {
@@ -38,186 +37,181 @@ class ChartsD3 extends Component {
 
   _updateInfo(e) {
     const county = e.target.getAttribute('name');
-    let tool = tooltipData.find(name => name.id === county);
+    const tool = tooltipData.find(name => name.id === county);
     this.props.dispatch(updateChoroInfo(tool));
   }
 
   render() {
     const { documents } = this.props;
-    var pieData = [
-      {label: 'Margarita', value: 20.0},
-      {label: 'John', value: 55.0},
-      {label: 'Tim', value: 25.0 }
-    ];
+    // const pieData = [
+    //   { label: 'Margarita', value: 20.0 },
+    //   { label: 'John', value: 55.0 },
+    //   { label: 'Tim', value: 25.0 },
+    // ];
 
     if (documents.length) {
-     let yearSnapshot = documents.reduce((previous,current) =>{
-      if(!current.dateCreated){
-        return previous;
-      }
-      if(previous[current.dateCreated.month]){
-        previous[current.dateCreated.month]++;
-      }else{
-        previous[current.dateCreated.month] = 1;
-      }
-      return previous;
-     }, {});
-      let months = Object.keys(yearSnapshot);
-      let values = months.map((month) => {
-          return {x:Number(month), y:yearSnapshot[month]};
-       });
-      this._lineDataPopulation = [{name:'series1', values: values}];
-    }else{
+      const yearSnapshot = documents.reduce((previous, current) => {
+        if (!current.dateCreated) {
+          return previous;
+        }
+        const _prev = { ...previous };
+        if (_prev[current.dateCreated.month]) {
+          _prev[current.dateCreated.month]++;
+        } else {
+          _prev[current.dateCreated.month] = 1;
+        }
+        return _prev;
+      }, {});
+      const months = Object.keys(yearSnapshot);
+      const values = months.map(month => ({ x: Number(month), y: yearSnapshot[month] }));
+      this._lineDataPopulation = [{ name: 'series1', values }];
+    } else {
       return null;
     }
 
-    //gather monthly average income
-    if (documents.length) {
-      let monthlyAverageIncome = {};
-
-      for(var monthIndex = 1; monthIndex < 12; monthIndex++){
-        const currentMonthDocs = documents
-        .filter(item =>{
-          const created = item.dateCreated;
-          return created ? (created.month === monthIndex) && (item.employmentStatus === 'yes') : false;
-        });
-        monthlyAverageIncome[monthIndex] = currentMonthDocs.reduce((prev, curr) => {
-          return prev + curr.employmentCurPay;
-        }, 0) / 2;
-      };
-      let months = Object.keys(monthlyAverageIncome);
-      let values = months.map((month) =>{
-        return {x:Number(month), y:monthlyAverageIncome[month]}
+    function getCurrentMonthDocs(monthIndex) {
+      return documents.filter(item => {
+        const created = item.dateCreated;
+        return created ? (created.month === monthIndex) && (item.employmentStatus === 'yes') : false;
       });
-      let averageIncome = [{name: 'Average Income', values: values}];
+    }
+
+    // gather monthly average income
+    if (documents.length) {
+      const monthlyAverageIncome = {};
+
+      for (var monthIndex = 1; monthIndex < 12; monthIndex++) {
+        const currentMonthDocs = getCurrentMonthDocs(monthIndex);
+
+        monthlyAverageIncome[monthIndex] = currentMonthDocs.reduce((prev, curr) => prev + curr.employmentCurPay, 0) / 2;
+      }
+      const months = Object.keys(monthlyAverageIncome);
+      const values = months.map(month => ({ x: Number(month), y: monthlyAverageIncome[month] }));
+      const averageIncome = [{ name: 'Average Income', values }];
       this._lineDataMonthlyIncome = averageIncome;
-    }else{
+    } else {
       return null;
     }
 
-    //gather 2 deep bar graph for non-shelter vs shelter homeless
+    // gather 2 deep bar graph for non-shelter vs shelter homeless
     if (documents.length) {
-      let yearSnapshot = documents.reduce((previous,current) =>{
-      if(!current.dateCreated){
-        return previous;
-      }
-      if(current.shelterStatus == 'no' && undefined) {
-        return previous;
-      }
-      if(previous[current.dateCreated.month]){
-        previous[current.dateCreated.month]++;
-      }else{
-        previous[current.dateCreated.month] = 1;
-      }
-      return previous;
-     }, {});
+      const yearSnapshot = documents.reduce((previous, current) => {
+        if (!current.dateCreated) {
+          return previous;
+        }
+        if (current.shelterStatus === 'no' && undefined) {
+          return previous;
+        }
+        const _prev = { ...previous };
+        if (_prev[current.dateCreated.month]) {
+          _prev[current.dateCreated.month]++;
+        } else {
+          _prev[current.dateCreated.month] = 1;
+        }
+        return _prev;
+      }, {});
 
-      let yearSnapshot2 = documents.reduce((previous, current) =>{
-      if(!current.dateCreated){
-        return previous;
-      }
-      if(current.shelterStatus == 'yes' && undefined) {
-        return previous;
-      }
-      if(previous[current.dateCreated.month]){
-        previous[current.dateCreated.month]++;
-      }else{
-        previous[current.dateCreated.month] = 1;
-      }
-      return previous;
-     }, {});
-      let months = Object.keys(yearSnapshot);
-      let values = months.map((month) => {
-          return {x:Number(month), y:yearSnapshot[month]};
-      });
-      let values2 = months.map((month) => {
-          return {x:Number(month), y:yearSnapshot2[month]};
-      });
-      console.log('values',values2);
-      console.log('values',values);
+      const yearSnapshot2 = documents.reduce((previous, current) => {
+        if (!current.dateCreated) {
+          return previous;
+        }
+        if (current.shelterStatus === 'yes' && undefined) {
+          return previous;
+        }
+        const _prev = { ...previous };
+        if (_prev[current.dateCreated.month]) {
+          _prev[current.dateCreated.month]++;
+        } else {
+          _prev[current.dateCreated.month] = 1;
+        }
+        return _prev;
+      }, {});
+      const months = Object.keys(yearSnapshot);
+      const values = months.map(month => ({ x: Number(month), y: yearSnapshot[month] }));
+      const values2 = months.map(month => ({ x: Number(month), y: yearSnapshot2[month] }));
+      // console.log('values',values2);
+      // console.log('values',values);
       this._barShelterData = [
         {
-          name:'series1',
-          values: values
+          name: 'series1',
+          values,
         },
         {
           name: 'series2',
-          values: values2
-        }
+          values: values2,
+        },
       ];
-    }else{
+    } else {
       return null;
     }
 
-    //Aggregate analytics for monthly income
+    // Aggregate analytics for monthly income
     if (documents.length) {
-      let benefitAlias = {
+      const benefitAlias = {
         benefitEbt: 'EBT',
         benefitSsi: '(SSI)',
         benefitTanf: '(TANF)',
         benefitUnemployment: 'Unemployment',
         benefitVeteran: 'Veteran',
-        benefitWelfare: 'Welfare'
+        benefitWelfare: 'Welfare',
       };
-      let benefitTracker = {
+      const benefitTracker = {
         benefitEbt: 0,
         benefitSsi: 0,
         benefitTanf: 0,
         benefitUnemployment: 0,
         benefitVeteran: 0,
-        benefitWelfare: 0
+        benefitWelfare: 0,
       };
 
-      documents.forEach((doc) =>{
-        (doc.benefitEbt == 'yes') ? benefitTracker.benefitEbt++ : '';
-        (doc.benefitSsi == 'yes') ? benefitTracker.benefitSsi++ : '';
-        (doc.benefitTanf == 'yes') ? benefitTracker.benefitTanf++ : '';
-        (doc.benefitUnemployment == 'yes') ? benefitTracker.benefitUnemployment++ : '';
-        (doc.benefitVeteran == 'yes') ? benefitTracker.benefitVeteran++ : '';
-        (doc.benefitWelfare == 'yes') ? benefitTracker.benefitWelfare++ : '';
+      documents.forEach(doc => {
+        if (doc.benefitEbt === 'yes') { benefitTracker.benefitEbt++; }
+        if (doc.benefitSsi === 'yes') { benefitTracker.benefitSsi++; }
+        if (doc.benefitTanf === 'yes') { benefitTracker.benefitTanf++; }
+        if (doc.benefitUnemployment === 'yes') { benefitTracker.benefitUnemployment++; }
+        if (doc.benefitVeteran === 'yes') { benefitTracker.benefitVeteran++; }
+        if (doc.benefitWelfare === 'yes') { benefitTracker.benefitWelfare++; }
       });
-      let benefitKeys = Object.keys(benefitTracker);
+      const benefitKeys = Object.keys(benefitTracker);
 
-      let allValueCount = benefitKeys.reduce((previous, current) =>{
-         return previous + benefitTracker[current]
-      },0);
+      const allValueCount = benefitKeys.reduce((previous, current) => previous + benefitTracker[current], 0);
 
-      this._pieDataBenefits = benefitKeys.map((key) => {
-        return {label: benefitAlias[key],  value: (Number(benefitTracker[key] / allValueCount * 100).toFixed(2))}
-      });
-    }else{
+      this._pieDataBenefits = benefitKeys.map(key => ({
+        label: benefitAlias[key],
+        value: (Number(benefitTracker[key] / allValueCount * 100).toFixed(2)),
+      }));
+    } else {
       return null;
     }
 
-    //Aggregate analytics for families
+    // Aggregate analytics for families
     if (documents.length) {
-      let familyAlias = {
+      const familyAlias = {
         familyMembersAdult: 'Adult',
         familyMembersChildren: 'Children',
       };
 
-      let familyTracker = {
+      const familyTracker = {
         familyMembersAdult: 0,
         familyMembersChildren: 0,
       };
 
-      documents.forEach((doc) =>{
-        if(!doc.familyMembersAdult) return;
-        if(!doc.familyMembersChildren) return;
+      documents.forEach(doc => {
+        if (!doc.familyMembersAdult) return;
+        if (!doc.familyMembersChildren) return;
         familyTracker.familyMembersChildren += doc.familyMembersChildren;
-        familyTracker.familyMembersAdult += doc.familyMembersAdult
+        familyTracker.familyMembersAdult += doc.familyMembersAdult;
       });
 
-      let familyKeys = Object.keys(familyTracker);
+      const familyKeys = Object.keys(familyTracker);
 
-      let allValueCount = familyKeys.reduce((previous, current) =>{
-         return previous + familyTracker[current]
-      },0);
+      const allValueCount = familyKeys.reduce((previous, current) => previous + familyTracker[current], 0);
 
-      this._pieDataFamilies = familyKeys.map((key) => {
-        return {label: familyAlias[key],  value: (Number(familyTracker[key] / allValueCount * 100).toFixed(2))}
-      });
-    }else{
+      this._pieDataFamilies = familyKeys.map(key => ({
+        label: familyAlias[key],
+        value: (Number(familyTracker[key] / allValueCount * 100).toFixed(2)),
+      }));
+    } else {
       return null;
     }
 
@@ -241,7 +235,7 @@ class ChartsD3 extends Component {
                 title="Overall Population for 2015"
                 yAxisLabel="Total Population"
                 xAxisLabel="Months"
-                gridHorizontal={true}
+                gridHorizontal
               />
             </Col>
             <Col xs={6}>
@@ -253,7 +247,7 @@ class ChartsD3 extends Component {
                 title="Average income for 2015"
                 yAxisLabel="Average Income"
                 xAxisLabel="Months"
-                gridHorizontal={true}
+                gridHorizontal
               />
             </Col>
           </Row>
@@ -264,9 +258,9 @@ class ChartsD3 extends Component {
                 width={450}
                 height={400}
                 fill={'#3182bd'}
-                title='Shelter vs Non-Shelter 2015'
-                yAxisLabel='Label'
-                xAxisLabel='Value'
+                title="Shelter vs Non-Shelter 2015"
+                yAxisLabel="Label"
+                xAxisLabel="Value"
               />
             </Col>
             <Col xs={6}>
